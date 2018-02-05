32fd5e34b3d19899da3a47d2dc5e3ba6084e0b74
Jasmine Santana <jasmine@Jasmines-MacBook-Pro.local>
2018-02-05 14:02:34 -0500

    Update

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES('Southwestern Burger');
+INSERT INTO burgers (burger_name) VALUES('Sushi Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES('Tiki Burger', true );   
